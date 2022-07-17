a18c30d510571fcb98966a3f7ed06591d4a09cc0
Teddyzander <edwardalexandersmall@hotmail.co.uk>
2022-07-18 09:38:59 +1000

    Create project

---
+++ data_util/data_util.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def gen_data(size):
+    """
+    Creates the data set
+    :param size: number of instances in the data set
+    :return: data
+    """
+
+    np.random.seed(123)
---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+size = 10000
+np.random.seed(123)
+
+
+
+
