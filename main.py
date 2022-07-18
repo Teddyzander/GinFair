7688e9cdcc1eafa3f5ea97824e3b5aad093e609a
Teddyzander <edwardalexandersmall@hotmail.co.uk>
2022-07-18 10:26:50 +1000

    Save sensitive data, remove from training

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ from sklearn.linear_model import LogisticRegression
 size = 1000
 data, target, sens = data_util.gen_data(1000)
 
+# save sensitive dat and then remove it from the data set
+sensitive = data[sens]
+data = data.drop(sens, axis=1)
+
 print('stop')
